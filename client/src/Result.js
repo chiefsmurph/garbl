9e68b0e7a8746cf244b170351a76422ddaaa5592
John Murphy <johnmurphy@Johns-MBP.lan1>
2020-11-22 20:43:54 -0800

    lets go

--- client/src/Result.js
+++ client/src/Result.js
@@ -1,8 +1,15 @@
-export default () => (
-    <div>
-        <h2>Result</h2>
-        <a href={`outputs/${window.location.href.split('?').pop()}`} download>Click to download</a><br/>
-        <a href='./'>Click to start over</a><br/>
+export default () => {
+    const audioUrl = `outputs/${window.location.href.split('?').pop()}`;
+    return (
+        <div>
+            <h2>Result</h2>
+            <audio controls preload="metadata" style={{ width: '300px' }}>
+                <source src={audioUrl} type="audio/mpeg" />
+                Your browser does not support the audio element.
+            </audio><br/><br/>
+            <a href={audioUrl} download>Click to download</a><br/><br/>
+            <a href='./'>Click to start over</a><br/>
 
-    </div>
-);
+        </div>
+    );
+}
