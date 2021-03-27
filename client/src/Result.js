c5fe5fe6610e683c4a109347b390f22c45b7cd54
chiefsmurph <chiefsmurph@gmail.com>
2021-03-27 13:48:56 -0400

    crazy breh

--- client/src/Result.js
+++ client/src/Result.js
@@ -2,12 +2,15 @@ export default () => {
     const file = window.location.hash.slice(4).split('?').slice(1).join('');
     const audioUrl = `outputs/${file}`;
     return (
-        <div>
-            <h2 className="result-box">Result: <i>{file}</i></h2>
-            <audio controls preload="metadata">
-                <source src={audioUrl} type="audio/mpeg" />
-                Your browser does not support the audio element.
-            </audio><br/><br/>
+        <div className="result-page">
+            <h2 className="result-box">result: <i>{file}</i></h2>
+            <div>
+                <audio controls preload="metadata">
+                    <source src={audioUrl} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                </audio>
+            </div>
+            <br/>
             <a href={audioUrl} download>Click to download</a><br/><br/>
             <a href='./'>Click to start over</a><br/>
 
